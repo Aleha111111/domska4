95e1c874f2575a7e5ce1376c7b16dab18227d73f
Ivan <le_ha@live.ru>
2019-02-20 11:12:59 +0500

    Вторая домашка

--- js/script.js
+++ js/script.js
@@ -28,6 +28,8 @@ window.addEventListener('DOMContentLoaded',()=>{const cartWrapper = document.que
                 removeBtn = document.createElement('div'),
                 empty =cartWrapper.querySelector('.empty');
             trigger.remove();
+            showConfirm();
+            calculate(1);
             removeBtn.classList.add('goods__item-remove');
             removeBtn.innerHTML = '&times';
             item.appendChild(removeBtn);
@@ -35,5 +37,63 @@ window.addEventListener('DOMContentLoaded',()=>{const cartWrapper = document.que
             if (empty){
                 empty.remove();
             }
+            calcTotal();
+            removeFromCart();
         });
-    });});
+
+    });
+    function sliceTitle(){titles.forEach(function (item) {
+        if(item.textContent.length < 70){
+            return;
+        } else {
+            const  str = item.textContent.slice(0,71)+'...';
+            item.textContent=str;
+
+        }
+    });}
+    sliceTitle();
+    function showConfirm() {
+        confirm.style.display='block';
+        let  counter =100;
+        const  id =setInterval(frame,10);
+        function frame(){
+            if (counter ==10){
+                clearInterval(id);
+                confirm.style.display='none';
+            }else {
+                counter--;
+                confirm.style.opacity = '.' + counter;
+                confirm.style.transform = 'translateY(-${counter}px)';
+            }
+        }
+    }
+    function calculate(i) {
+        const items =cartWrapper.querySelectorAll('.goods__item');
+        badge.textContent=items.length+i;
+        if (items.length+i ==0){
+            const cler = document.createElement('div');
+            cler.innerHTML='Ваша корзина пока пуста';
+            cler.className='empty';
+            cartWrapper.appendChild(cler);
+
+        }
+    }
+    function calcTotal() {
+        const  prises =document.querySelectorAll('.cart__wrapper >.goods__item>.goods__price>span');
+        let total=0;
+        prises.forEach(function(item){
+            total+=+item.textContent;
+        });
+        totalCost.textContent=total;
+    }
+function  removeFromCart() {
+    const removeBtn =cartWrapper.querySelectorAll('.goods__item-remove');
+    removeBtn.forEach(function (btn) {
+        btn.addEventListener('click',()=>{
+            btn.parentElement.remove();
+            calculate(0);
+            calcTotal();
+        });
+    });
+}
+});
